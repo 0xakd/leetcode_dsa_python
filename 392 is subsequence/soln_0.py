0729532d5e93bfa498a7a46f9287078806e86770
Ajay <ajay@ADs-MacBook-Air.local>
2026-04-12 19:18:06 +0530

    392 is Subsequence

---
+++ 392 is subsequence/soln_0.py
@@ -0,0 +1,15 @@
+def isSubsequence(s: str, t: str) -> bool:
+    itr1, itr2 = 0, 0
+    
+    while itr1 < len(s) and itr2 < len(t):
+        if s[itr1] == t[itr2]:
+            itr1 += 1
+            itr2 += 1
+        else:
+            itr2 +=1
+    return itr1 == len(s)
+
+s = "abaaca"
+t = "ahbgdc"
+
+print(isSubsequence(s, t))
